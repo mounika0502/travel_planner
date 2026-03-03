dd5dbb5d54ab11b1bee4bb32c538140d8411a958
Mounika Prasanth <mounika0502@gmail.com>
2026-03-03 10:46:35 +0530

    Fixed axios production baseURL

--- travelfrontend/src/api/axios.js
+++ travelfrontend/src/api/axios.js
@@ -1,17 +1,26 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "https://travel-planner-8akq.onrender.com/api/"
+      : "http://127.0.0.1:8000/api/",
 });
 
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem("access");
+// Attach JWT token automatically
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
 
 export default API;
